7ac8be7903ea0b54df3871b376205d5dde7c8897
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-11-06 14:40:00 -0800

    day 1 complete

---
+++ script.js
@@ -0,0 +1,11 @@
+// single line 
+
+/*double line
+double line
+double line*/
+
+let name = "Royce Fenwick" ;
+console.log(name);
+
+let favoriteFood = "hamburger rice skillet";
+console.log(favoriteFood);
