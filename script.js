8c42a03fd9e53583fd352e8a4d3f6155a0a9fc4d
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-11-06 15:18:38 -0800

    actually completed day 2 challanege

--- script.js
+++ script.js
@@ -13,4 +13,9 @@ console.log(favoriteFood);
 // day-2 challenge
 
 let words = 'Supercalifragilisticexpialidocious';
-console.log(words.length)
+console.log(words.length);
+
+let number = 13498723479875439873457897233347987234879n
+console.log(typeof number);
+let numbers = 39024320982745873248793
+console.log(typeof numbers);
