2d3297fb009c0a2e9e326a240480ffad6cf5e041
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-11-06 15:03:19 -0800

    completed day 2 challenge

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ console.log(name);
 
 let favoriteFood = "hamburger rice skillet";
 console.log(favoriteFood);
+
+// day-2 challenge
+
+let words = 'Supercalifragilisticexpialidocious';
+console.log(words.length)
